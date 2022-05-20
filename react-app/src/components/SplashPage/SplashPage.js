a4db39694fdaf296601505ecfc5d6c8f66ec4811
Nathan Zavala <zavalanathana@gmail.com>
2022-05-19 23:48:16 -0700

    Splash Page added slogan and title logo

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -8,7 +8,10 @@ function SplashPage(){
     <>
       <div id="splash-page-container">
         <img src='https://res.cloudinary.com/doulyb7dt/image/upload/v1652983342/DojoDeals/logo1_h16vkt.png' alt="enso-logo" id="splash-image-enso"/>
-        <div>Please <NavLink to='/login'>Log In</NavLink> or <NavLink to='/sign-up'>Sign Up</NavLink> to continue...</div>
+        <img src='https://res.cloudinary.com/doulyb7dt/image/upload/v1653028512/DojoDeals/dojodealsgray_hmuzgg.png' alt='dojo-deals' id="splash-page-dojo" />
+        <div id="splash-page-slogan-one">MARTIAL ARTS SUPPLY</div>
+        <div id="splash-page-slogan-two">Don't Fight A Good Deal</div>
+        <div id="continue-path">Please <NavLink to='/login'>Log In</NavLink> or <NavLink to='/sign-up'>Sign Up</NavLink> to continue...</div>
       </div>
     </>
   )
